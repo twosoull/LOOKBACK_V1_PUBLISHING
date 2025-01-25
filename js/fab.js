6947bb534eb0e17232b3589614f81e7b18b0c79e
iyeonghun <twosoull@naver.com>
2025-01-25 17:42:35 +0900

    화면 수정

--- js/fab.js
+++ js/fab.js
@@ -2,13 +2,14 @@ const addButton = document.querySelector('.add-btn');
 const actionMenu = document.querySelector('#actionMenu');
 
 // + 버튼 클릭 이벤트
-addButton.addEventListener('click', () => {
+addButton.addEventListener('click', (e) => {
+  e.stopPropagation(); // 이벤트 전파 중단
   actionMenu.classList.toggle('show'); // 메뉴 표시/숨기기 토글
 });
 
 // 문서 다른 곳 클릭 시 메뉴 숨기기
 document.addEventListener('click', (e) => {
-  if (!addButton.contains(e.target) && !actionMenu.contains(e.target)) {
+  if (!actionMenu.contains(e.target)) {
     actionMenu.classList.remove('show');
   }
 });
